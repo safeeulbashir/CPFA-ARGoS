1383c03f52a504ecffa1686c9e550070143584c5
safeeulbashir <safee_cuet06@yahoo.com>
2016-06-10 17:43:53 -0600

    [Change Type] Adding EvolutionCommand

--- evolve_EXAMPLE.sh
+++ evolve_EXAMPLE.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # First argument is the number of processes to generate
 
-nohup bash -c "mpirun -n $1 -machinefile $2 ~/GitHub/CPFA-ARGoS/build/cpfa_evolver -t 10 -p 50 -g 100 -c 0.1 -m 0.1 -s 0.2 -e 1 -x experiments/ExampleEvolution.xml seed 1111 && cat example_evolution_output.txt | mail -s \"Example Evolution Description\" example@domain.com" > evolve_clustered.out &
+nohup bash -c "mpirun -n $1 -machinefile $2 ~/Documents/ForEvaluation/CPFA-ARGoS/build/cpfa_evolver -t 90 -p 50 -g 100 -c 0.1 -m 0.1 -s 0.2 -e 1 -x experiments/CPFAExampleEvolution.xml seed 1111 && cat example_evolution_output.txt  > evolve_clustered.out &
 
