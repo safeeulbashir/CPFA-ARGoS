f7a351404e04fd6502c21f36ab3dbf26cfd3abde
safeeulbashir <safee_cuet06@yahoo.com>
2016-07-25 13:55:02 -0600

    [Change Type] Change Point Detection Algorithm Added

---
+++ pyscripts/ChangePointCreator.py
@@ -0,0 +1,121 @@
+import sys
+import glob
+import os
+import re
+import datetime
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import csv
+import scipy.signal
+import errno
+import subprocess
+import signal
+# Example Call
+# python pyscripts/ChangePointCreator.py -w 60 -m 10 -l 5400 -d 1 -c 4 CPFA_saves/2016-06-28_18-37-00
+# -w =Window Size
+# -m sliding amount
+# -l Experiment Length
+# -d Distribution Type
+# -c Number of change points
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='CUSUM plotter')
+    parser.add_argument('-w', '--window', action='store', dest='window_size', type=int)
+    parser.add_argument('-m', '--movement', action='store', dest='slide_movement',type=int)
+    parser.add_argument('-l', '--length', action='store', dest='length', type=int)
+    parser.add_argument('-d', '--pile', action='store', dest='pile', type=int)
+    parser.add_argument('-c', '--changePoints', action='store', dest='changePoints', type=int)
+    parser.add_argument('directory',help='directory to use',action='store')
+    args = parser.parse_args()
+    length = 5400
+    if args.length:
+        length = args.length
+
+    window_size = 60
+    if args.window_size:
+        window_size = args.window_size
+
+    slide_movement = 10
+    if args.slide_movement:
+        slide_movement = args.slide_movement
+    
+    change_points=4
+    if args.changePoints:
+        change_points=args.changePoints 
+        
+
+    pile = 1
+    if args.pile:
+        pile = args.pile
+
+    # print sys.argv[11] for printing the directory
+    key_types = {'Distribution Type': int,
+    'Ant ID': int,
+    'Pile ID': int,
+    'Y-Position': float,
+    'Drop Off Time': float,
+    'Pickup Time': float,
+    'X-Position': float}
+
+    path = sys.argv[11]+"CPFARecording"
+    pathForPheromone=sys.argv[11]+"PheromoneRecordings/"
+    # print pathForPheromone
+    dirs = os.listdir( path )
+    # For Saving Sliding Windows
+    dirname = "./"+sys.argv[11]+"FilesForRScript"
+    try:
+        os.makedirs(dirname)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(dirname):
+            pass
+        else:
+                raise()
+    #For Creating a directory to save plots
+    dirname = "./"+sys.argv[11]+datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')+"_w"+str(window_size)+"_l"+str(length)+"_s"+str(slide_movement)+"_c"+str(change_points)+"_p"+str(pile)
+    try:
+        os.makedirs(dirname)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(dirname):
+            pass
+        else:
+                raise
+    #print dirname
+    # This would print all the files and directories
+    for file in dirs:
+        #print file
+        splitter= file.split("_")
+        #print splitter[0]
+        with open(path+"/"+file, 'r') as csvfile:
+            reader = csv.DictReader(csvfile.readlines(), delimiter="\t")
+        
+        reader = list(reader)
+        data = {}
+        for key in key_types:
+            data[key] = []
+            for row in reader:
+                data[key].append(key_types[key](row[key]))
+            
+        dist_types = set(data['Distribution Type'])
+
+    
+        collection_times = {}
+        sliding_windows = {}
+        for d in dist_types:
+            collection_times[d] = np.zeros(length)
+            sliding_windows[d] = []
+    
+        for d, t in zip(data['Distribution Type'], data['Drop Off Time']):
+            if t >= 0:
+                t = t 
+                collection_times[d][t] += 1
+    
+        for i in xrange(0, int(length / slide_movement)):
+            slmax = min(length,window_size + i * slide_movement)
+            for d in dist_types:
+                sl = collection_times[d][(i*slide_movement):slmax]
+                sliding_windows[d].append(np.sum(sl))
+        saveInto="./"+sys.argv[11]+"FilesForRScript/"+splitter[0]+"_"+splitter[1]+".txt"
+        np.savetxt(saveInto,sliding_windows[pile])
+        proc=subprocess.Popen(["Rscript", "pyscripts/ChangePointDetector.R",saveInto,str(change_points),str(slide_movement),pathForPheromone,str(pile),dirname])
+
+
---
+++ pyscripts/ChangePointDetector.R
@@ -0,0 +1,108 @@
+library(changepoint)
+library(pracma)
+args <- commandArgs()
+args
+mydata=read.table(args[6])
+distributionType=as.numeric(args[10])
+data<-as.vector(mydata[,1])
+cumsum1=cumsum(data)
+cumsum1=detrend(cumsum1, 'linear')
+cumsum=binseg.mean.cusum(cumsum1,Q=as.numeric(args[7]),pen=2)
+cps=cumsum['cps']
+cumsum1=cumsum(data)
+cumsum1=detrend(cumsum1, 'linear')
+xCordinate<-list()
+yCordinate<-list()
+for(i in seq(1,length(cps[[1]]), 2)){
+	xCordinate[i]<-cps[[1]][i]	
+	yCordinate[i]<-cumsum1[as.numeric(unlist(cps[[1]][i]))]
+}
+xCordinate<-as.numeric(unlist(xCordinate))
+yCordinate<-as.numeric(unlist(yCordinate))
+argsplit=strsplit(args[6],"[.]")
+argsplit2=strsplit(argsplit[[1]][[2]],"[/]")
+#For Pheromone Data
+#PeromoneLayingTime=read.table("./PheromoneLayTiming.txt")
+
+randomseed=strsplit(argsplit2[[1]][[5]],"[_]")
+#layingTimeInSlidingWindow=SimTimeFromTable/(ticksPerSecond*SlidingWindowMovement)
+##LayingTime=PeromoneLayingTime[PeromoneLayingTime[,1]==randomseed[[1]][[2]],2]/(16*as.numeric(args[8]))
+#randomseed
+########################################
+#Colors for Diffrent Types of Pheromones
+########################################
+colors=c("darkgreen","aquamarine","chartreuse4","chocolate1","blue","cyan3","blueviolet","brown4","darkgoldenrod4","darkorchid3","deeppink4","orangered","sienna","salmon","green4","khaki4")
+
+##################################
+#Get the file for Pheromone Lists
+##################################
+inputFile <- paste(args[9],argsplit2[[1]][[5]],".pheromone",sep="")
+con  <- file(inputFile, open = "r")
+pheromoneList <- list() #Contails the pheromone lists as for individual Pile
+while (length(oneLine <- readLines(con, n = 1, warn = FALSE)) > 0) {
+    myVector <- (strsplit(oneLine, " "))
+    myVector <- list(as.numeric(myVector[[1]]))
+    pheromoneList <- c(pheromoneList,myVector)
+  } 
+pheromoneList
+imagename=paste(argsplit2[[1]][[5]],"png",sep=".")
+imagename=paste("/",imagename,sep="")
+imagename=paste(args[11],imagename,sep="")
+png(filename=imagename)
+plot(cumsum1,col="red",lty=1,lwd=2,type="l",main = paste("Plot For ",argsplit2[[1]][[5]],sep=""), xlab = paste(args[8]," Seconds Sliding Window",sep=""), ylab = "Cumulative Sum")
+points(xCordinate,yCordinate,pch=23)
+#legend("topright", inset=.05,c("Change Points in Seconds"), fill=terrain.colors(3), horiz=TRUE)
+text(xCordinate, yCordinate, xCordinate*as.numeric(args[8]), cex=0.6, pos=4, col="red")
+########################################################
+#Determine the positions of Pheromones in sliding window
+########################################################
+colorIndex=1
+lowrange=0
+upperrange=0
+if(distributionType==1){
+	lowrange=1
+	upperrange=1
+}
+if(distributionType==4){
+	lowrange=2
+	upperrange=5
+} 
+if(distributionType==16){
+	lowrange=6
+	upperrange=21
+}
+if(upperrange!=0 && lowrange!=0){
+	for(count in lowrange:upperrange)
+	{
+		tempListXCord<-list()
+		tempListYCord<-list()
+		valcounter=0
+		for(pheromones in pheromoneList[count])
+		{ skipper=0
+			for(pheromone in pheromones)
+			{
+				if(skipper>2 && pheromone/(as.numeric(args[8]))>=1)
+				{
+					tempListXCord[valcounter]=pheromone/(as.numeric(args[8]))
+					tempListYCord[valcounter]=cumsum1[pheromone/(as.numeric(args[8]))]
+					#print(pheromone/(16*as.numeric(args[8])))
+					valcounter=valcounter+1	
+				}
+				else skipper=skipper+1
+				
+
+				
+			}
+			
+		}
+		tempListXCord<-unlist(tempListXCord)
+		tempListYCord<-unlist(tempListYCord)
+		points(tempListXCord,tempListYCord,col=colors[colorIndex])
+		colorIndex=colorIndex+1
+		#print(length(tempListXCord))
+	}
+} 
+##points(LayingTime,cumsum1[LayingTime],pch=21)
+##text(LayingTime, cumsum1[LayingTime], LayingTime*as.numeric(args[8]), cex=0.6, pos=4, col="green")
+dev.off()
+
