21fb3234adb969288c5fa4d67bc5af7694571290
safeeulbashir <safee_cuet06@yahoo.com>
2016-07-29 17:39:59 -0600

    [Change Type] False Positive and True Positive is detection Code is added

--- pyscripts/Stats.py
+++ pyscripts/Stats.py
@@ -8,7 +8,8 @@ import csv
 #python pyscripts/Stats.py CPFA_saves/2016-07-25_13-23-18/2016-07-26_16-57-49_w60_l5400_s10_c4_p1 16 ##
 #######################################################################################################
 
-DifferenceInSeconds=list()
+TruePositive=list()
+FalsePositive=list()
 pathToCPs="./"+sys.argv[1]+"/ChangePoints.txt"
 Directory=str.split(sys.argv[1],"/")
 pathToPheromoneRecordingDir="./"+Directory[0]+"/"+Directory[1]+"/"+"PheromoneRecordings/"
@@ -29,38 +30,47 @@ with open(pathToCPs, 'r') as csvfile:
 csvfile.close()
 count=0;
 for randomseed,cp1,cp2,cp3,cp4 in zip(ChangePoints['RandomSeed'],ChangePoints['CP1'],ChangePoints['CP2'],ChangePoints['CP3'],ChangePoints['CP4']):
-	#print str(randomseed)+" "+str(cp1)+" "+str(cp2)+" "+str(cp3)+" "+str(cp4)
 	pheromones=list()
 	np.array(pheromones)
-	DifferenceInSeconds.append([])
-	DifferenceInSeconds[count].append(randomseed)
+	FalsePositive.append([])
+	TruePositive.append([])
+	TruePositive[count].append(randomseed)
+	FalsePositive[count].append(randomseed)
 	pathToPheromoneFile=pathToPheromoneRecordingDir+"RandomSeed_"+str(randomseed)+".pheromone"
-	print randomseed
 	f = open(pathToPheromoneFile,'r')
 	for line in f.readlines():
 		line=str.split(line," ")
 		if(line[1]==sys.argv[2]):		
-			#DifferenceInSeconds[count].append(cp1)
 			for x in range(2, len(line)):
-				print line[x]
+				#print line[x]
 				if(line[x]!='\n'):
 					pheromones.append(line[x])
 	f.close()
 	cps=np.array([cp1,cp2,cp3,cp4])
-	#print cps
 	for cp in cps:	# Adding the values less then each change point in an arrays
 		tempPheromones=list() #values those are less then a cp
+		tempPheromones2=list()#Values used for false positive
 		for pheromone in pheromones:
 			if(int(pheromone)<=int(cp)): #conditional check
 				tempPheromones.append(int(pheromone))
 		tempPheromones=np.array(tempPheromones)
 		tempPheromones=np.sort(tempPheromones)
-		#print tempPheromones
 		if(len(tempPheromones)>0): #checks if no pheromone laying event is there before the change point.
 			diff=cp-tempPheromones[len(tempPheromones)-1]
-		else: diff=-1
-		#print diff
-		DifferenceInSeconds[count].append(diff)
+			falseP=-1
+		else: 
+			diff=-1
+			for pheromone in pheromones: # for changepoints that occures before pheromone laying event.
+				if(int(pheromone)>=int(cp)): #conditional check
+					tempPheromones2.append(int(pheromone))
+			tempPheromones2=np.array(tempPheromones2)
+			tempPheromones2=np.sort(tempPheromones2)
+			if(len(tempPheromones2)>0): # Check if there is any false positive points
+				falseP=tempPheromones2[0]-cp #the smallest value is at 0th Index
+			else: falseP=-1
+		FalsePositive[count].append(falseP)
+		TruePositive[count].append(diff)
+
 	count=count+1
-	#break
-print DifferenceInSeconds
+print TruePositive
+print FalsePositive
